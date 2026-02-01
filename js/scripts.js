f96e1cf21c8b6e96ef041f2e4b57f966cdf0b200
Vitaliy Sulimenko <prevetal@gmail.com>
2026-02-01 13:03:04 +0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,116 @@
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	// History slider
+	const historySliders = [],
+		history = document.querySelectorAll('.history .swiper')
+
+	history.forEach((el, i) => {
+		el.classList.add('history_s' + i)
+
+		let options = {
+			loop: true,
+			loopAdditionalSlides: 2,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			lazy: true,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			slidesPerView: 'auto',
+			spaceBetween: 24,
+		}
+
+		historySliders.push(new Swiper('.history_s' + i, options))
+	})
+
+
+	// Mob. menu
+	$('.mob_header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('.mob_header .mob_menu_btn').toggleClass('active')
+		$('body').toggleClass('lock')
+		$('.mob_menu').toggleClass('show')
+	})
+
+
+	// Phone input mask
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+
+
+	if (is_touch_device()) {
+		const subMenus = document.querySelectorAll('header .menu .sub')
+
+		// Submenu on the touch screen
+		$('header .menu .item > a.sub_link').click(function (e) {
+			const dropdown = $(this).next()
+
+			if (dropdown.css('visibility') === 'hidden') {
+				e.preventDefault()
+
+				subMenus.forEach(el => el.classList.remove('show'))
+				dropdown.addClass('show')
+
+				BODY.style = 'cursor: pointer;'
+			}
+		})
+
+		// Close the submenu when clicking outside it
+		document.addEventListener('click', e => {
+			if ($(e.target).closest('.menu').length === 0) {
+				subMenus.forEach(el => el.classList.remove('show'))
+
+				BODY.style = 'cursor: default;'
+			}
+		})
+	}
+
+
+	// Solutions
+	setHeight(document.querySelectorAll('.solutions .item .name'))
+	setHeight(document.querySelectorAll('.solutions .item .solution .desc'))
+	setHeight(document.querySelectorAll('.solutions .item .result .desc'))
+
+
+	// Mob. footer
+	$('footer .data .title').click(function(e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active').next().slideToggle(300)
+	})
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || BODY.clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Overwrite window width
+		WW = window.innerWidth || document.clientWidth || BODY.clientWidth
+
+
+		// Solutions
+		setHeight(document.querySelectorAll('.solutions .item .name'))
+		setHeight(document.querySelectorAll('.solutions .item .solution .desc'))
+		setHeight(document.querySelectorAll('.solutions .item .result .desc'))
+	}
+})
