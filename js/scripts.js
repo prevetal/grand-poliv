5953af5cd3ca4bd1fa556c73c91f2cfaeba292f3
Vitaliy Sulimenko <prevetal@gmail.com>
2026-02-03 11:44:26 +0400

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,49 @@ document.addEventListener('DOMContentLoaded', function() {
 		$('.mob_header .mob_menu_btn').toggleClass('active')
 		$('body').toggleClass('lock')
 		$('.mob_menu').toggleClass('show')
+
+		$('.mob_header .mob_menu_btn').hasClass('active')
+			? $('.overlay').fadeIn(300)
+			: $('.overlay').fadeOut(200)
+	})
+
+
+	$('.mob_menu .head .close_btn, .overlay').click((e) => {
+		e.preventDefault()
+
+		$('.mob_header .mob_menu_btn').removeClass('active')
+		$('body').removeClass('lock')
+		$('.mob_menu').removeClass('show')
+
+		$('.overlay').fadeOut(200)
+	})
+
+
+	$('.mob_menu .menu .item > a.sub_link').click(function(e) {
+		e.preventDefault()
+
+		$(this).next('.sub').addClass('show')
+	})
+
+
+	$('.mob_menu .menu .sub .back_btn').click(function(e) {
+		e.preventDefault()
+
+		$(this).closest('.sub').removeClass('show')
+	})
+
+
+	$('.mob_menu .search_btn').click(function(e) {
+		e.preventDefault()
+
+		$('.mob_menu .mob_search').addClass('show')
+	})
+
+
+	$('.mob_menu .mob_search .back_btn').click(function(e) {
+		e.preventDefault()
+
+		$('.mob_menu .mob_search').removeClass('show')
 	})
 
 
@@ -94,6 +137,80 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		$(this).toggleClass('active').next().slideToggle(300)
 	})
+
+
+	// Search
+	$('header .search_btn').click(function(e) {
+		e.preventDefault()
+
+		$(this).addClass('active')
+		$('body').addClass('lock')
+
+		$(this).hasClass('active')
+			? $('.search').fadeIn(300)
+			: $('.search').fadeOut(200)
+	})
+
+
+	$('.search .close_btn').click(function(e) {
+		e.preventDefault()
+
+		$('header .search_btn').removeClass('active')
+		$('body').removeClass('lock')
+
+		$('.search').fadeOut(200)
+	})
+
+
+	$(document).on('click', function (e) {
+		if (
+			$(e.target).closest('.search').length &&
+			!$(e.target).closest('.cont').length
+		) {
+			$('header .search_btn').removeClass('active')
+			$('body').removeClass('lock')
+
+			$('.search').fadeOut(200)
+		}
+	})
+
+
+	// Fix. header
+	headerInit = true,
+	headerHeight = $('header').outerHeight()
+
+	$('header').wrap('<div class="header_wrap"></div>')
+	$('.header_wrap').height(headerHeight)
+
+	headerInit && $(window).scrollTop() > 0
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
+
+
+	// Fix. mob. header
+	mobHeaderInit = true,
+	mobHeaderHeight = $('.mob_header').outerHeight()
+
+	$('.mob_header').wrap('<div class="mob_header_wrap"></div>')
+	$('.mob_header_wrap').height(mobHeaderHeight)
+
+	mobHeaderInit && $(window).scrollTop() > 0
+		? $('.mob_header').addClass('fixed')
+		: $('.mob_header').removeClass('fixed')
+})
+
+
+
+window.addEventListener('scroll', () => {
+	// Fix. header
+	typeof headerInit !== 'undefined' && headerInit && $(window).scrollTop() > 0
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
+
+	// Fix. mob. header
+	typeof mobHeaderInit !== 'undefined' && mobHeaderInit && $(window).scrollTop() > 0
+		? $('.mob_header').addClass('fixed')
+		: $('.mob_header').removeClass('fixed')
 })
 
 
@@ -112,5 +229,37 @@ window.addEventListener('resize', function () {
 		setHeight(document.querySelectorAll('.solutions .item .name'))
 		setHeight(document.querySelectorAll('.solutions .item .solution .desc'))
 		setHeight(document.querySelectorAll('.solutions .item .result .desc'))
+
+
+		// Fix. header
+		headerInit = false
+		$('.header_wrap').height('auto')
+
+		setTimeout(() => {
+			headerInit = true
+			headerHeight = $('header').outerHeight()
+
+			$('.header_wrap').height(headerHeight)
+
+			headerInit && $(window).scrollTop() > 0
+				? $('header').addClass('fixed')
+				: $('header').removeClass('fixed')
+		}, 100)
+
+
+		// Fix. mob. header
+		mobHeaderInit = false
+		$('.mob_header_wrap').height('auto')
+
+		setTimeout(() => {
+			mobHeaderInit = true
+			mobHeaderHeight = $('.mob_header').outerHeight()
+
+			$('.mob_header_wrap').height(mobHeaderHeight)
+
+			mobHeaderInit && $(window).scrollTop() > 0
+				? $('.mob_header').addClass('fixed')
+				: $('.mob_header').removeClass('fixed')
+		}, 100)
 	}
 })
